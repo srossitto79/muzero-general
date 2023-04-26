0155ca8feb3c614291c12bbd457199959fb9c0d7
Salvatore Rossitto <srossitto79@gmail.com>
2023-04-27 00:45:40 +0200

    ...

--- games/trading.py
+++ games/trading.py
@@ -52,7 +52,7 @@ class MuZeroConfig:
         ### Training
         self.results_path = pathlib.Path(__file__).resolve().parents[1] / "results" / pathlib.Path(__file__).stem / datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")  # Path to store the model weights and TensorBoard logs
         self.save_model = True  # Save the checkpoint in results_path as model.checkpoint
-        self.training_steps = 10000 # Total number of training steps (ie weights update according to a batch)
+        self.training_steps = 1000 # Total number of training steps (ie weights update according to a batch)
         self.checkpoint_interval = 10  # Number of training steps before using the model for self-playing
         self.value_loss_weight = 1  # Scale the value loss to avoid overfitting of the value function, paper recommends 0.25 (See paper appendix Reanalyze)
         self.train_on_gpu = torch.cuda.is_available()  # Train on GPU if available
